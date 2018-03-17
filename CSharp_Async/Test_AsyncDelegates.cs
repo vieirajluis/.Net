114dca42eca21544f68af5c367d91ca38628edc6
joaolsvieira <joaolsvieira@gmail.com>
2018-03-17 18:45:28 -0400

    edit

---
+++ CSharp_Async/Test_AsyncDelegates.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Diagnostics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestingDelegates
+{
+    [TestClass] 
+    public class Test_AsyncDelegates
+    {
+        private void DoWork()
+        {
+            Debug.WriteLine("Hello World1");
+            Debug.WriteLine(System.Threading.Thread.CurrentThread.ManagedThreadId.ToString());
+        }
+
+        delegate void DoWorkDelegate();
+
+        [TestMethod]
+        public void Demo01()
+        {
+           // DoWork();
+            Debug.WriteLine(System.Threading.Thread.CurrentThread.ManagedThreadId.ToString());
+            DoWorkDelegate m = new DoWorkDelegate(DoWork);
+            m.Invoke();
+            AsyncCallback callback = new AsyncCallback(TheCallback);
+            IAsyncResult ar = m.BeginInvoke(callback, m);
+            //// do more 
+
+            ar.AsyncWaitHandle.WaitOne();
+        }
+
+        private static void TheCallback(IAsyncResult ar)
+        {
+            var m = ar.AsyncState as DoWorkDelegate;
+            m.EndInvoke(ar); // this is where you use try/catch
+        }
+    }
+}
---
+++ CSharp_Async/UnitTestAsyncDatabaseReader.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProjectDatabase
+{
+    [TestClass]
+    public class Test_Databases
+    {
+        //[TestMethod]
+        //public void Test_DB_Sync()
+        //{
+        //    string connectionString;
+        //    #region Assign connectionString
+        //    connectionString = "Data Source=(localdb)\\ProjectsV12;Initial Catalog=master;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+        //    #endregion
+
+        //    string sqlSelect = "SELECT @@VERSION";
+        //    using (var sqlConnection = new SqlConnection(connectionString))
+        //    {
+        //        sqlConnection.Open();
+        //        using (var sqlCommand = new SqlCommand(sqlSelect, sqlConnection))
+        //        {
+        //            using (var reader = sqlCommand.ExecuteReader())
+        //            {
+        //                while(reader.Read())
+        //                {
+        //                    var data = reader[0].ToString();
+        //                }
+        //            }
+        //        }
+        //    }
+        //}
+
+        [TestMethod]
+        public void Test_DB_ASync()
+        {
+            string connectionString;
+            #region Assign connectionString
+            connectionString = "Data Source=(localdb)\\ProjectsV12;Initial Catalog=master;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+            #endregion
+
+            string sqlSelect = "SELECT @@VERSION";
+           
+
+            using (var sqlConnection = new SqlConnection(connectionString))
+            {
+                sqlConnection.Open();
+                var sqlCommand = new SqlCommand(sqlSelect, sqlConnection);
+                var callback = new AsyncCallback(DataAvailable);
+                var ar = sqlCommand.BeginExecuteReader(callback, sqlCommand);
+
+                
+
+                ar.AsyncWaitHandle.WaitOne();
+            }
+        }
+
+        private static void DataAvailable(IAsyncResult ar)
+        {
+            var sqlCommand = ar.AsyncState as SqlCommand;
+            using (var reader = sqlCommand.EndExecuteReader(ar))
+            {
+                while (reader.Read())
+                {
+                    var data = reader[0].ToString();
+                }
+            }
+        }
+    
+    }
+}
